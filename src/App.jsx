a98faa33e0b881cc83dd456bf91dfb93d13470f0
thechrisvazquez <thechrisvazquez@gmail.com>
2025-12-02 15:58:12 -0600

    72

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,19 @@ import { db } from "./data/db"
 function App() {
 
   const [data, setData] = useState(db)
+  const [cart, setCart] = useState([])
+
+  function addToCart(item) {
+    const itemExist = cart.findIndex(guitar => guitar.id === item.id)
+    if (itemExist === -1) {
+      item.quantity = 1
+      setCart([...cart, item])
+    } else {
+      const updateCart = [...cart]
+      updateCart[itemExist].quantity++
+      setCart(updateCart)
+    }
+  }
 
   return (
     <>
@@ -13,7 +26,7 @@ function App() {
       <main className="container-xl mt-5">
         <div className="row mt-5">
           {data.map((guitar) => (
-            <Guitar key={guitar.id} guitar={guitar} />
+            <Guitar key={guitar.id} guitar={guitar} setCart={setCart} addToCart={addToCart} />
           ))}
         </div>
       </main>
--- src/components/Guitar.jsx
+++ src/components/Guitar.jsx
@@ -1,7 +1,4 @@
-export default function Guitar({ guitar }) {
-    const handleClick = (guitar) => {
-        console.log(guitar)
-    }
+export default function Guitar({ guitar, addToCart }) {
 
     return (
         <div className="col-md-6 col-lg-4 my-4 row align-items-center">
@@ -15,7 +12,7 @@ export default function Guitar({ guitar }) {
                 <button
                     type="button"
                     className="btn btn-dark w-100"
-                    onClick={() => handleClick(guitar)}
+                    onClick={() => addToCart(guitar)}
                 >Agregar al Carrito</button>
             </div>
         </div>
